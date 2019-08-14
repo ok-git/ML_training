7d4141d3ea60b7b1408df80172b50588256a8be3
Oleg <olka.contact@gmail.com>
2019-08-14 14:40:36 +0700

    Сделал прогноз на моделях LinearRegression и KNeighborsRegressor(n_neighbors=7)

--- ml_training.py
+++ ml_training.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 from pandas.plotting import register_matplotlib_converters
 
 register_matplotlib_converters()
@@ -9,8 +11,60 @@ register_matplotlib_converters()
 money = pd.read_csv("c:\Temp\money.csv")
 
 # plt.plot(money["date"], money["value"])
-plt.plot(money["value"])
+# plt.plot(money["value"])
 # plt.xticks(rotation=45)
+# plt.show()
+
+past = 7*4
+future = 7*1
+
+df = list()
+values = money["value"]
+for i in range(past, len(money)-future):
+    part_of_values = values[(i-past):(i+future)]
+    df.append(list(part_of_values))
+
+#for el in df:
+#    print(el, "\n")
+
+past_columns = [f"past_{i+1}" for i in range(past)]
+future_columns = [f"future_{i+1}" for i in range(future)]
+
+df = pd.DataFrame(df, columns=(past_columns + future_columns))
+print(df.head(5))
+
+X = df[past_columns][:-1]
+y = df[future_columns][:-1]
+
+X_test = df[past_columns][-1:]
+y_test = df[future_columns][-1:]
+
+print(y_test, "\n")
+
+reg = LinearRegression().fit(X, y)
+
+prediction = reg.predict(X_test)[0]
+
+print(prediction, "\n")
+print(prediction - y_test, "\n")
+print(np.linalg.norm(prediction - y_test), "\n")
+
+plt.plot(prediction, label="Prediction")
+plt.plot(df[future_columns].iloc[-1], label="real")  # iloc - select an element from specific position
+plt.legend()
 plt.show()
 
+# ---------------
 
+reg = KNeighborsRegressor(n_neighbors=7).fit(X, y)
+
+prediction = reg.predict(X_test)[0]
+
+print(prediction, "\n")
+print(prediction - y_test, "\n")
+print(np.linalg.norm(prediction - y_test), "\n")
+
+plt.plot(prediction, label="Prediction")
+plt.plot(df[future_columns].iloc[-1], label="real")  # iloc - select an element from specific position
+plt.legend()
+plt.show()
